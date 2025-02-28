bc4b9d662fb97cf15850a5cd0575ade09943215c
Arun-clover <arunkumarkarna.2104@gmail.com>
2025-02-28 12:07:09 +0530

    commited

--- server.js
+++ server.js
@@ -25,6 +25,10 @@ app.get('/page4(.html)?',(req,res)=>{
     res.sendFile(path.join(__dirname,'public','html','page5.html'));
  })
 
+ app.get('/*',(req,res)=>{
+   res.status(404).sendFile(path.join(__dirname,'public','html','404.html'));
+ })
+
  app.listen(port,()=>{
      console.log(`Server is running on port http://localhost:${port}`);
 
